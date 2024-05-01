75bbbd1fecc3d7ad541f7339392faf3619efb456
ragab2005 <148394949+ragab2005@users.noreply.github.com>
2024-05-01 08:19:09 +0300

    Add files via upload

--- AnonXMusic/plugins/play/rdod.py
+++ AnonXMusic/plugins/play/rdod.py
@@ -8,7 +8,7 @@ from config import OWNER_ID, SUPPORT_CHANNEL, SUPPORT_CHAT
 
 async def huhh(client: Client, message: Message): 
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/ce26e7b9ee1310efdaa1a.jpg",
+        photo=f"https://telegra.ph/file/550cad095c1da597bcb47.jpg",
         caption=f"• 𝗧𝗵𝗲 𝗕𝗲𝘀𝘁 𝗦𝗼𝘂𝗿𝗰𝗲 𝗢𝗻 𝗧𝗲𝗹𝗲𝗴𝗮𝗺 🎸 .",
         reply_markup=InlineKeyboardMarkup(
             [
