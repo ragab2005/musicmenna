0a19408051baf93e287494b9235214d4f7ecf8cf
ragab2005 <148394949+ragab2005@users.noreply.github.com>
2024-05-13 19:22:19 +0300

    Add files via upload

--- AnonXMusic/utils/decorators/must_join.py
+++ AnonXMusic/utils/decorators/must_join.py
@@ -14,7 +14,9 @@ def must_join_ch(zohary):
         i = await get_must(app.username)
         s = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMember?chat_id=@{i}&user_id={msg.from_user.id}")
         data = s.json()
-        if not data["ok"] and data["result"]["status"] not in ["member","creator","administrator"]:
+        if data["ok"] and data["result"]["status"] in ["member","creator","administrator"]:
+          return await zohary(c,msg)
+        else:
           return await msg.reply(f"- يجب الاشتراك بالقناه اولا لاستخدام البوت \n - @{i} .",disable_web_page_preview=True,reply_markup=ikm([[ikb("اضغط للاشتراك بالقناه.",url=f"https://t.me/{i}")],]))
     return await zohary(c,msg)
   return ch_user
