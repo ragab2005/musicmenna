9004f7547de124a1152611188233d5cefecd6379
ragab2005 <148394949+ragab2005@users.noreply.github.com>
2024-06-02 21:13:45 +0300

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import os
 import re
 import textwrap
@@ -8,8 +17,8 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import YOUTUBE_IMG_URL, OWNER_ID 
-from AnonXMusic import app
+from config import YOUTUBE_IMG_URL, OWNER_ID
+from AnonX import app
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -20,7 +29,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def get_thumb(videoid):
+async def gen_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 
@@ -56,9 +65,10 @@ async def get_thumb(videoid):
                     )
                     await f.write(await resp.read())
                     await f.close()
+        owner = OWNER_ID[0]
         wxyz = await app.download_media(
-            (await app.get_users(int(OWNER_ID))).photo.big_file_id,
-              file_name=f"{OWNER_ID}.jpg",
+              (await app.get_users(owner)).photo.big_file_id,
+              file_name=f"{owner}.jpg",
         )
         wxy = Image.open(wxyz)
         youtube = Image.open(f"cache/thumb{videoid}.png")
@@ -78,10 +88,10 @@ async def get_thumb(videoid):
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 70)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("assets/font.ttf", 30)
+        font = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 40)
+        font2 = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 70)
+        arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
+        name_font = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
