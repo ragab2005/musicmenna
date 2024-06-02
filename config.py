a76388f3058d98f829403cdcb9c0a2d585725d46
ragab2005 <148394949+ragab2005@users.noreply.github.com>
2024-06-02 21:15:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/anoexmusic/mahmoudfaragaiden",
+    "https://github.com/ragab2005/musicmenna",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
