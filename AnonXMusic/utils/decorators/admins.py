8b53cb86cb17e70ca1ba74d13bb7bb239e29a3f8
ragab2005 <148394949+ragab2005@users.noreply.github.com>
2024-06-15 18:04:49 +0300

    Update admins.py

--- AnonXMusic/utils/decorators/admins.py
+++ AnonXMusic/utils/decorators/admins.py
@@ -173,7 +173,7 @@ def ActualAdminCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.username} احنا صايمين يصحبي اتقي الله وروح صلي  قناة السورس .",
+                    f"{app.mention} احنا صايمين يصحبي اتقي الله وروح صلي  قناة السورس .",
                     show_alert=True,
                 )
         try:
